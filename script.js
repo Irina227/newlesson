c311db9b72e75d250964b4b57c5908787fb87c93
Ira Korytko <irichkakorytko@yandex.ru>
2022-08-10 11:57:59 +0300

    мой первый коммит

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/*УРОК 5*/
+let potato = 10;
+
+// const peelPotato = function(num) {
+//     return 'Картошка №' + num + ' почищена'
+// }
+// console.log(peelPotato(1));
+
+/*const peelPotato = function(potatoIndex){
+    if (potatoIndex >= -10) {
+        console.log('Картошка №' + potatoIndex + ' почищена');
+        potatoIndex--
+        peelPotato(potatoIndex);
+    }
+}
+peelPotato(potato)*/ //рекурсия
+
+
+// for(let i = 10; i > 0; i--) {
+//     console.log('Картошка №' + i + ' почищена');
+// } //цикл for
+
+// while (potato > 0){
+//     console.log('Картошка №' + potato + 'почищена');
+//     potato--
+// }
+
+// do{
+//     console.log('Картошка №' + potato + 'почищена');
+//     potato--
+// } while (potato > 0)
+
+/*for(let i = 10; i > 0; i--) {
+    if (i === 5) continue
+     console.log('Картошка №' + i + ' почищена');
+} */
