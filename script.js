e7bba0c0f5b75524c6dda50c342f0639726a8fdd
Ira Korytko <irichkakorytko@yandex.ru>
2022-08-11 10:53:23 +0300

    урок 5

--- script.js
+++ script.js
@@ -1,91 +1,63 @@
-'use strict';
+"use strict";
 
 /*УРОК 5*/
-// let potato = 10;
 
-// const peelPotato = function(num) {
-//     return 'Картошка №' + num + ' почищена'
-// }
-// console.log(peelPotato(1));
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько бкдет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 
-/*const peelPotato = function(potatoIndex){
-    if (potatoIndex >= -10) {
-        console.log('Картошка №' + potatoIndex + ' почищена');
-        potatoIndex--
-        peelPotato(potatoIndex);
-    }
-}
-peelPotato(potato)*/ //рекурсия
+let rollback = 10;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
 
-
-// for(let i = 10; i > 0; i--) {
-//     console.log('Картошка №' + i + ' почищена');
-// } //цикл for
-
-// while (potato > 0){
-//     console.log('Картошка №' + potato + 'почищена');
-//     potato--
-// }
-
-// do{
-//     console.log('Картошка №' + potato + 'почищена');
-//     potato--
-// } while (potato > 0)
-
-/*for(let i = 10; i > 0; i--) {
-    if (i === 5) continue
-     console.log('Картошка №' + i + ' почищена');
-} */
-
-
-
-let title = prompt('Как называется ваш проект?')
-let screens = prompt('Какие типы экранов нужно разработать?')
-let screenPrice = +prompt('Сколько бкдет стоить данная работа?')
-let adaptive = confirm('Нужен ли адаптив на сайте?')
-
-let rollback = 10
-let allServicePrices
-let fullPrice
-let servicePercentPrice
-
-let service1 = prompt('Какой дополнительный тип услуги нужен?')
-let servicePrice1 = +prompt('Сколько это будет стоить?')
-let service2 = +prompt('Какой дополнительный тип услуги нужен?')
-let servicePrice2 = +prompt('Сколько это будет стоить?')
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = +prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
 
 const getAllServicePrices = function () {
-    return servicePrice1 + servicePrice2
-}
+  return servicePrice1 + servicePrice2;
+};
 const showTypeOf = function (variable) {
-    console.log(variable, typeof variable);
+  console.log(variable, typeof variable);
+};
+
+const getFullPrice = function () {
+    return screenPrice + allServicePrices
 }
+
 const getServicePercentPrice = function () {
-    return fullprice - (fullPrice * (rollback / 100))
-}
+  return fullPrice - fullPrice * (rollback / 100);
+};
+
 const getTitle = function () {
-    return title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase()
-}
+  return (
+    title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase()
+  );
+};
+
 const getRollbackMessage = function (price) {
-    if (price >= 30000){
-        return "Даем скидку в 10%"
-    } else if (price >= 15000 && price < 30000){
-        return "Даем скидку в 5%"
-    } else if (price >= 0 && price < 15000 ) {
-        return "Скидка не предусмотрена"
-    } else {
-        return "Что-то пошло не так"
-    }
-}
+  if (price >= 30000) {
+    return "Даем скидку в 10%";
+  } else if (price >= 15000 && price < 30000) {
+    return "Даем скидку в 5%";
+  } else if (price >= 0 && price < 15000) {
+    return "Скидка не предусмотрена";
+  } else {
+    return "Что-то пошло не так";
+  }
+};
 
-allServicePrices = getAllServicePrices()
-fullPrice = getFullPrice()
-servicePercentPrice = getServicePercentPrice()
-title = getTitle()
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrice();
+title = getTitle();
 
-showTypeOf(title)
-showTypeOf(screenPrice)
-showTypeOf(adaptive)
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
 
 console.log(getRollbackMessage(fullPrice));
 console.log(typeof title);
@@ -95,4 +67,10 @@ console.log(typeof adaptive);
 console.log(screens.length);
 console.log(servicePercentPrice);
 
-console.log("Стоимость верстки экранов " + screenPrice + "юани и стоимость разработки сайта " + fullPrice + "юани");
+console.log(
+  "Стоимость верстки экранов " +
+    screenPrice +
+    "юани и стоимость разработки сайта " +
+    fullPrice +
+    "юани"
+);
