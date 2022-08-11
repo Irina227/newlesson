e54e7b85eacac59dec0e6f8855b29e46e8493e3e
Ira Korytko <irichkakorytko@yandex.ru>
2022-08-11 10:08:52 +0300

    новый 4 урок

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
 'use strict';
 
-/*УРОК 5*/
-let potato = 10;
+/*УРОК 4*/
+let title = prompt('Как называется ваш проект?')
+let screens = prompt('Какие типы экранов нужно разработать?')
+let screenPrice = +prompt('Сколько бкдет стоить данная работа?')
+let adaptive = confirm('Нужен ли адаптив на сайте?')
 
-// const peelPotato = function(num) {
-//     return 'Картошка №' + num + ' почищена'
-// }
-// console.log(peelPotato(1));
+let rollback = 10
+let allServicePrices
+let fullPrice
+let servicePercentPrice
 
-/*const peelPotato = function(potatoIndex){
-    if (potatoIndex >= -10) {
-        console.log('Картошка №' + potatoIndex + ' почищена');
-        potatoIndex--
-        peelPotato(potatoIndex);
+let service1 = prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice1 = +prompt('Сколько это будет стоить?')
+let service2 = +prompt('Какой дополнительный тип услуги нужен?')
+let servicePrice2 = +prompt('Сколько это будет стоить?')
+
+const getAllServicePrices = function () {
+    return servicePrice1 + servicePrice2
+}
+const showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+}
+const getServicePercentPrice = function () {
+    return fullprice - (fullPrice * (rollback / 100))
+}
+const getTitle = function () {
+    return title.trim()[0].toUpperCase() + title.trim().substring(1).toLowerCase()
+}
+const getRollbackMessage = function (price) {
+    if (price >= 30000){
+        return "Даем скидку в 10%"
+    } else if (price >= 15000 && price < 30000){
+        return "Даем скидку в 5%"
+    } else if (price >= 0 && price < 15000 ) {
+        return "Скидка не предусмотрена"
+    } else {
+        return "Что-то пошло не так"
     }
 }
-peelPotato(potato)*/ //рекурсия
 
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice()
+servicePercentPrice = getServicePercentPrice()
+title = getTitle()
 
-// for(let i = 10; i > 0; i--) {
-//     console.log('Картошка №' + i + ' почищена');
-// } //цикл for
+showTypeOf(title)
+showTypeOf(screenPrice)
+showTypeOf(adaptive)
 
-// while (potato > 0){
-//     console.log('Картошка №' + potato + 'почищена');
-//     potato--
-// }
+console.log(getRollbackMessage(fullPrice));
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
 
-// do{
-//     console.log('Картошка №' + potato + 'почищена');
-//     potato--
-// } while (potato > 0)
+console.log(screens.length);
+console.log(servicePercentPrice);
 
-/*for(let i = 10; i > 0; i--) {
-    if (i === 5) continue
-     console.log('Картошка №' + i + ' почищена');
-} */
+console.log("Стоимость верстки экранов " + screenPrice + "юани и стоимость разработки сайта " + fullPrice + "юани");
