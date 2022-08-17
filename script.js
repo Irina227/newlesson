7755336dd7f3a7b9e7dc4e33f9b67aacbf549553
Ira Korytko <irichkakorytko@yandex.ru>
2022-08-17 13:29:20 +0300

    7урок

--- script.js
+++ script.js
@@ -1,23 +1,103 @@
 "use strict";
 
-/*УРОК 6*/
-let number = prompt("Угадай число от 1 до 100");
+/*УРОК 7*/
 
-const countSum = function () {
-  if (number > 100) {
-    console.log("Загаданное число больше");
-    number = prompt("Введи новый вариант");
-    console.log(number);
-  } else if (number < 100 && number > 1) {
-    console.log("Загаданное число меньше");
-    number = prompt("Введи новый вариант");
-    console.log(number);
-  } else if (isNaN(number) || number === null) {
-    alert("Введи число");
-    number = prompt("Введи новый вариант");
-    console.log("Игра окончена");
-  } else if (number = 100) {
-    console.log("Поздравляю, Вы угадали!!!");
-  }
-};
-countSum();
+
+const appData = {
+    title: '',
+    screens: '',
+    screenPrice: 0,
+    adaptive: true,
+    rollback: 10,
+    allServicePrices: 0,
+    fullPrice: 0,
+    servicePercentPrice: 0,
+    service1: '',
+    service2: '',
+    asking: function () {
+      appData.title = prompt("Как называется ваш проект?", "Калькулятор верстки");
+      appData.screens = prompt("Какие типы экранов нужно разработать?", "5000");
+    
+      do {
+        appData.screenPrice = prompt("Сколько будет стоить данная работа?");
+      } while (!isNumber(appData.screenPrice));
+    
+      appData.adaptive = confirm("Нужен ли адаптив на сайте?");
+    }, 
+
+    isNumber: function (num) {
+      return !isNaN(parseFloat(num)) && isFinite(num);
+    },
+
+    getAllServicePrices: function () {
+      let sum = 0;
+    
+      for (let i = 0; i < 2; i++) {
+        let price = 0;
+    
+        if (i === 0) {
+          appData.service1 = prompt("Какой дополнительный тип услуги нужен?");
+        } else if (i === 1) {
+          appData.service2 = prompt("Какой дополнительный тип услуги нужен?");
+        }
+        do {
+          price = prompt("Сколько это будет стоить?");
+        } while (!isNumber(price));
+    
+        sum += +price;
+      }
+    
+      return sum;
+    },
+
+    getFullPrice: function () {
+      return +appData.screenPrice + appData.allServicePrices;
+    },
+
+    getServicePercentPrice: function () {
+      return appData.fullPrice - appData.fullPrice * (appData.rollback / 100);
+    },
+    getServicePercentPrice: function () {
+      return appData.fullPrice - appData.fullPrice * (appData.rollback / 100);
+    },
+
+    getTitle: function () {
+      return (
+        appData.title.trim()[0].toUpperCase() + appData.title.trim().substring(1).toLowerCase()
+      );
+    },
+
+    getRollbackMessage: function (price) {
+      if (price >= 30000) {
+        return "Даем скидку в 10%";
+      } else if (price >= 15000 && price < 30000) {
+        return "Даем скидку в 5%";
+      } else if (price >= 0 && price < 15000) {
+        return "Скидка не предусмотрена";
+      } else {
+        return "Что-то пошло не так";
+      }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+appData.asking();
+appData.allServicePrices = getAllServicePrices();
+appData.fullPrice = getFullPrice();
+appData.servicePercentPrice = getServicePercentPrice();
+appData.title = getTitle();
+
+console.log(appData.fullPrice);
+console.log(appData.servicePercentPrice);
+
