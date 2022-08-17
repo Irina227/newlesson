4c000fab4e5ef188f6b19237561c8351538cb51a
Ira Korytko <irichkakorytko@yandex.ru>
2022-08-17 13:58:51 +0300

    домашка 6 урок

--- script.js
+++ script.js
@@ -5,19 +5,26 @@ let number = prompt("Угадай число от 1 до 100");
 
 const countSum = function () {
   if (number > 100) {
-    console.log("Загаданное число больше");
-    number = prompt("Введи новый вариант");
-    console.log(number);
-  } else if (number < 100 && number > 1) {
     console.log("Загаданное число меньше");
     number = prompt("Введи новый вариант");
-    console.log(number);
-  } else if (isNaN(number) || number === null) {
+    console.log('Новый вариант ' + number);
+  } ;
+
+  if (number < 1 ) {
+    console.log("Загаданное число больше");
+    number = prompt("Введи новый вариант");
+    console.log('Новый вариант ' + number);
+  };
+    
+  if (isNaN(number) || number === null) {
     alert("Введи число");
     number = prompt("Введи новый вариант");
     console.log("Игра окончена");
-  } else if (number = 100) {
+  };
+
+  if (number = 100) {
     console.log("Поздравляю, Вы угадали!!!");
-  }
+  };
+
 };
 countSum();
